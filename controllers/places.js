1e665ae68a0fe329e9f10d9ae24dd5bd7469f42b
Nika <sscott004@citymail.cuny.edu>
2022-12-14 20:56:17 -0500

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,20 @@
 const router = require('express').Router()
 const places = require('../models/places')
 
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id] })
+
+  }
+})
+
 router.get('/new', (req, res) => {
     res.render('places/new')
   })
@@ -38,20 +52,7 @@ router.post('/', (req, res) => {
     res.redirect('/places')
   })
   
-  router.get('/:id', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
-      res.render('error404')
-    }
-    else if (!places[id]) {
-      res.render('error404')
-    }
-    else {
-      res.render('places/show', { place: places[id] })
 
-    }
-  })
-  
    
 
 module.exports = router
